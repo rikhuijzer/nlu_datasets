5c8997ab1420e293d06070620127f2b3154ea473
rikhuijzer <t.h.huijzer@student.tue.nl>
2018-12-15 19:15:01 +0100

    fixed second bug in merge_spans

--- src/converter.py
+++ src/converter.py
@@ -13,7 +13,7 @@ from nltk.tokenize import WordPunctTokenizer
 
 def convert_message_to_annotated_str(message: Message) -> str:
     """Convert Message object to string having annotated entities."""
-    # message.text = message.text.replace('(', '').replace(')', '').replace('[', '').replace(']', '')
+    message.text = message.text.replace('(', '').replace(')', '').replace('[', '').replace(']', '')
     training_examples = [message]
     training_data = TrainingData(training_examples=training_examples)
     generated = MarkdownWriter()._generate_training_examples_md(training_data)
@@ -31,6 +31,26 @@ def convert_message_line(message: Message) -> Tuple:
         return base_row
 
 
+def write_tsv(tuples: Iterable[Tuple], n_cols: int, filename: Path):
+    """Helper for to_tsv. Writes tuples to file."""
+    with open(str(filename), 'w', encoding='utf8', newline='') as tsv_file:
+        tsv_writer = csv.writer(tsv_file, delimiter='\t', lineterminator='\n')
+        header = ['Annotated sentence', 'Intent']
+        if n_cols == 3:
+            header.append('Training')
+        tsv_writer.writerow(header)
+        for row in tuples:
+            tsv_writer.writerow([*row])
+
+
+def to_tsv(corpus: Corpus, filename: Path):
+    """Write corpus to tsv file specified by Path."""
+    messages = get_messages(corpus)
+    n = 3 if 'training' in messages[0].data else 2
+    tuples = map(convert_message_line, messages)
+    write_tsv(tuples, n, filename)
+
+
 def annotate_entity_tokens(entity: dict) -> List[str]:
     """Create NER annotations for given entity."""
     span_generator = WordPunctTokenizer().span_tokenize(entity['value'])
@@ -46,16 +66,22 @@ def merge_spans(spans: List[Tuple], entities: List[dict]) -> List[Tuple]:
     end_indexes = list(map(lambda e: e['end'], entities))
     merged = []
     start = 0
+    skip = False
     for span in spans:
-        if span[0] in start_indexes:
+        start_match = span[0] in start_indexes
+        end_match = span[1] in end_indexes
+        if start_match:
             start = span[0]
-        elif span[1] in end_indexes:
+            skip = True
+        if end_match:
             merged.append((start, span[1]))
-        else:
+            skip = False
+        if not (start_match or end_match or skip):
             merged.append(span)
     return merged
 
 
+# i need a connection from [harras](StationStart) to [karl-preis-platz](StationDest) at [8 am](TimeStartTime).
 def annotate_tokens(spans: List[Tuple], entities: List[dict]) -> List[str]:
     """Use entities to create NER annotations for given spans."""
     merged_spans = merge_spans(spans, entities)
@@ -78,29 +104,38 @@ def convert_message_lines(message: Message) -> str:
     spans = [span for span in span_generator]
     tokens = list(map(lambda t: message.text[t[0]:t[1]], spans))
     annotations = annotate_tokens(spans, message.data['entities'])
+    assert len(tokens) == len(annotations)
     lines = list(map(lambda t: '{} {}'.format(t[0], t[1]), zip(tokens, annotations)))
     lines = '\n'.join(lines)
     return lines
 
 
-def write_tsv(tuples: Iterable[Tuple], n_cols: int, filename: Path):
-    with open(str(filename), 'w', encoding='utf8', newline='') as tsv_file:
-        tsv_writer = csv.writer(tsv_file, delimiter='\t', lineterminator='\n')
-        header = ['Annotated sentence', 'Intent']
-        if n_cols == 3:
-            header.append('Training')
-        tsv_writer.writerow(header)
-        for row in tuples:
-            tsv_writer.writerow([*row])
+def convert_messages_lines(messages: Iterable[Message]) -> str:
+    """Convert multiple messages to lines which can be stored in well-known NER format."""
+    return '\n\n'.join(map(lambda message: convert_message_lines(message), messages))
 
 
-def to_tsv(corpus: Corpus, filename: Path):
+def write(text: str, filename: Path):
+    """Writes entire file at once based on text."""
+    with open(str(filename), 'w', encoding='utf8') as f:
+        f.write(text)
+
+
+def write_filtered_ner(messages: Iterable[Message], filename: Path, training: bool):
+    """Writes train or test messages to filename."""
+    filtered_messages = filter(lambda message: message.data['training'] == training, messages)
+    write(convert_messages_lines(filtered_messages), filename)
+
+
+def write_ner(corpus: Corpus, folder: Path):
+    """Writes complete corpus to train and test files."""
     messages = get_messages(corpus)
-    n = 3 if 'training' in messages[0].data else 2
-    tuples = map(convert_message_line, messages)
-    write_tsv(tuples, n, filename)
+    write_filtered_ner(messages, folder / 'train.txt', training=True)
+    write_filtered_ner(messages, folder / 'test.txt', training=False)
 
 
 if __name__ == '__main__':
-    dataset = Corpus.SNIPS2017
-    to_tsv(dataset, get_project_root() / 'data' / dataset.name.lower() / (dataset.name.lower() + '.tsv'))
+    dataset = Corpus.CHATBOT
+    folder = get_project_root() / 'generated' / dataset.name.lower()
+    write_ner(dataset, folder)
+    # to_tsv(dataset, folder / (dataset.name.lower() + '.tsv'))
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,6 +1,8 @@
 from src.utils import create_message
 from src.my_types import Corpus
-from src.converter import convert_message_lines, annotate_tokens, annotate_entity_tokens, merge_spans
+from src.converter import (
+    convert_message_lines, annotate_tokens, annotate_entity_tokens, merge_spans, convert_messages_lines
+)
 
 message = create_message(text='Alternative to Facebook Messenger.',
                          intent='Find Alternative',
@@ -10,20 +12,34 @@ message = create_message(text='Alternative to Facebook Messenger.',
                                     'value': 'Facebook Messenger'}],
                          training=False,
                          corpus=Corpus.MOCK)
-
 entities = message.data['entities']
 spans = [(0, 11), (12, 14), (15, 23), (24, 33), (33, 34)]
+lines = [
+    'Alternative O',
+    'to O',
+    'Facebook B-WebService',
+    'Messenger I-WebService',
+    '. O'
+]
 
 
 def test_annotate_entity_tokens():
     entity = message.data['entities'][0]
     expected = ['B-WebService', 'I-WebService']
-    assert expected == annotate_entity_tokens(entity)
+    assert annotate_entity_tokens(entity) == expected
 
 
 def test_merge_spans():
     expected = [(0, 11), (12, 14), (15, 33), (33, 34)]
-    assert expected == merge_spans(spans, entities)
+    assert merge_spans(spans, entities) == expected
+
+
+def test_merge_spans_advanced():
+    test_spans = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7)]
+    test_entities = [{'start': 1, 'end': 3}, {'start': 3, 'end': 6}]
+    actual = merge_spans(test_spans, test_entities)
+    expected = [(0, 1), (1, 3), (3, 6), (6, 7)]
+    assert actual == expected
 
 
 def test_annotate_tokens():
@@ -32,16 +48,45 @@ def test_annotate_tokens():
 
 
 def test_convert_message_lines():
-    expected = [
-        'Alternative O',
-        'to O',
-        'Facebook B-WebService',
-        'Messenger I-WebService',
-        '. O'
-    ]
-    expected = '\n'.join(expected)
+    expected = '\n'.join(lines)
     assert convert_message_lines(message) == expected
 
 
-def test_to_ner_file():
-    raise NotImplementedError()
+def test_convert_messages_lines():
+    expected = '\n\n'.join(map(lambda i: '\n'.join(lines), range(3)))
+    messages = [message for i in range(3)]
+    assert convert_messages_lines(messages) == expected
+
+
+def test_bug_omitted_entity():
+    # i need a connection from [harras](StationStart) to [karl-preis-platz](StationDest) at [8 am](TimeStartTime).
+    test_message = create_message(text='i need a connection from harras to karl-preis-platz at 8 am.',
+                                  intent='Find Connection',
+                                  entities=[
+                                      {'end': 51, 'entity': 'StationDest', 'start': 35, 'value': 'karl-preis-platz'},
+                                      {'end': 59, 'entity': 'TimeStartTime', 'start': 55, 'value': '8 am'},
+                                      {'end': 31, 'entity': 'StationStart', 'start': 25, 'value': 'harras'}
+                                  ],
+                                  training=False,
+                                  corpus=Corpus.MOCK)
+
+    expected = '\n'.join([
+        'i O',
+        'need O',
+        'a O',
+        'connection O',
+        'from O',
+        'harras B-StationStart',
+        'to O',
+        'karl B-StationDest',
+        '- I-StationDest',
+        'preis I-StationDest',
+        '- I-StationDest',
+        'platz I-StationDest',
+        'at O',
+        '8 B-TimeStartTime',
+        'am I-TimeStartTime',
+        '. O'
+    ])
+    actual = convert_message_lines(test_message)
+    assert actual == expected
