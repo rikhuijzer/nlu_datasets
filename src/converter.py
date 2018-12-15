bbd8a9553ca114e83a1dc00960e37633324afb6c
rikhuijzer <t.h.huijzer@student.tue.nl>
2018-12-15 15:48:35 +0100

    created convert_message_lines

--- src/converter.py
+++ src/converter.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-from typing import Tuple, Iterable
+from typing import Tuple, Iterable, List
 
 from rasa_nlu.training_data import TrainingData
 from rasa_nlu.training_data.formats.markdown import MarkdownWriter
@@ -8,6 +8,7 @@ from rasa_nlu.training_data.message import Message
 
 from src.my_types import Corpus
 from src.utils import get_project_root, get_messages
+from nltk.tokenize import WordPunctTokenizer
 
 
 def convert_message_to_annotated_str(message: Message) -> str:
@@ -28,6 +29,58 @@ def convert_message_line(message: Message) -> Tuple:
         return (*base_row, message.data['training'])
     else:
         return base_row
+
+
+def annotate_entity_tokens(entity: dict) -> List[str]:
+    """Create NER annotations for given entity."""
+    span_generator = WordPunctTokenizer().span_tokenize(entity['value'])
+    spans = [span for span in span_generator]
+    entity_name = entity['entity']
+    annotations = map(lambda i: ('B-' if i == 0 else 'I-') + entity_name, range(len(spans)))
+    return list(annotations)
+
+
+def merge_spans(spans: List[Tuple], entities: List[dict]) -> List[Tuple]:
+    """Merge spans which are covered by some entity."""
+    start_indexes = list(map(lambda e: e['start'], entities))
+    end_indexes = list(map(lambda e: e['end'], entities))
+    merged = []
+    start = 0
+    for span in spans:
+        if span[0] in start_indexes:
+            start = span[0]
+        elif span[1] in end_indexes:
+            merged.append((start, span[1]))
+        else:
+            merged.append(span)
+    return merged
+
+
+def annotate_tokens(spans: List[Tuple], entities: List[dict]) -> List[str]:
+    """Use entities to create NER annotations for given spans."""
+    merged_spans = merge_spans(spans, entities)
+    start_indexes = list(map(lambda e: e['start'], entities))
+    annotations = []
+    for span in merged_spans:
+        start = span[0]
+        if start in start_indexes:
+            entity = list(filter(lambda e: e['start'] == start, entities))[0]
+            annotations.append(annotate_entity_tokens(entity))
+        else:
+            annotations.append('O')
+    annotations = [item for sublist in annotations for item in sublist]  # flatten list
+    return annotations
+
+
+def convert_message_lines(message: Message) -> str:
+    """Convert message to lines which can be stored in txt in the well-known NER format."""
+    span_generator = WordPunctTokenizer().span_tokenize(message.text)
+    spans = [span for span in span_generator]
+    tokens = list(map(lambda t: message.text[t[0]:t[1]], spans))
+    annotations = annotate_tokens(spans, message.data['entities'])
+    lines = list(map(lambda t: '{} {}'.format(t[0], t[1]), zip(tokens, annotations)))
+    lines = '\n'.join(lines)
+    return lines
 
 
 def write_tsv(tuples: Iterable[Tuple], n_cols: int, filename: Path):
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,1 +1,47 @@
+from src.utils import create_message
+from src.my_types import Corpus
+from src.converter import convert_message_lines, annotate_tokens, annotate_entity_tokens, merge_spans
 
+message = create_message(text='Alternative to Facebook Messenger.',
+                         intent='Find Alternative',
+                         entities=[{'end': 33,
+                                    'entity': 'WebService',
+                                    'start': 15,
+                                    'value': 'Facebook Messenger'}],
+                         training=False,
+                         corpus=Corpus.MOCK)
+
+entities = message.data['entities']
+spans = [(0, 11), (12, 14), (15, 23), (24, 33), (33, 34)]
+
+
+def test_annotate_entity_tokens():
+    entity = message.data['entities'][0]
+    expected = ['B-WebService', 'I-WebService']
+    assert expected == annotate_entity_tokens(entity)
+
+
+def test_merge_spans():
+    expected = [(0, 11), (12, 14), (15, 33), (33, 34)]
+    assert expected == merge_spans(spans, entities)
+
+
+def test_annotate_tokens():
+    expected = ['O', 'O', 'B-WebService', 'I-WebService', 'O']
+    assert expected == annotate_tokens(spans, entities)
+
+
+def test_convert_message_lines():
+    expected = [
+        'Alternative O',
+        'to O',
+        'Facebook B-WebService',
+        'Messenger I-WebService',
+        '. O'
+    ]
+    expected = '\n'.join(expected)
+    assert convert_message_lines(message) == expected
+
+
+def test_to_ner_file():
+    raise NotImplementedError()
